641805ffed0fa83ccc025296d3e96c7a4e4f1a48
malinhanak <malinehanak@gmail.com>
2015-03-31 11:07:46 +0200

    navupdate

--- inc/nav.php
+++ inc/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-default">
-  <div class="container-fluid">
+  <div id="main-nav" class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -8,31 +8,24 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+  
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li><a href="index.php">Hem</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li class="shirts">><a href="#">T-shirts</a></li>
-            <li class="contact"><a href="#">Kontakt</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
+        <li><a href="#">Om oss</a></li>
+        <li><a href="#">T-shirts</a></li>
       </ul>
+      
       <ul class="nav navbar-nav navbar-right">
-        <li class="contact"><a href="#">Kontakt</a></li>
+        <li><a href="#">Kontakt</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Mitt inlogg <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Mitt Inlogg<span class="caret"></span></a>
+          <ul id="navshadow" class="dropdown-menu" role="menu">
             <li><a href="#">Inställningar</a></li>
-            <li class="chart"><a href="#">Varukorg</a></li>
+            <li><a href="homebreds.php">Varukorg</a></li>
             <li class="divider"></li>
             <li><a href="#">Logga ut</a></li>
           </ul>

a922eb4c69eb44e6557dda8071b9cf3d268999dc
malinhanak <malinehanak@gmail.com>
2015-04-08 11:47:27 +0200

    ff

--- shirts.php
+++ shirts.php
@@ -20,36 +20,31 @@ if($res->num_rows > 0)
 	while($row = $res->fetch_object())
 	{ ?>
 	<div class="col-xs-6 col-md-3">
+		<div class="thumbnail">
 		<h2><?php echo $row->name; ?></h2>
 		<p><?php echo $row->description; ?></p>
 		<p><?php echo $row->price; ?> kr</p>
-		<a href="#" class="thumbnail">
+	
 		<img class='img-responsive' src="<?php echo $row->img_src; ?>">
-		</a>
+		</div>
 	</div> <!-- col end -->
-	<div class="col-xs-6 col-md-3">
+	<div class="col-md-4">
 		<h2>title</h2>
 		<p>desc</p>
 		<p>0 kr</p>
-		<a href="#" class="thumbnail">
 		<img class='img-responsive' src="http://placehold.it/200x200">
-		</a>
 	</div> <!-- col end -->
-	<div class="col-xs-6 col-md-3">
+	<div class="col-md-4">
 		<h2>title</h2>
 		<p>desc</p>
 		<p>0 kr</p>
-		<a href="#" class="thumbnail">
 		<img class='img-responsive' src="http://placehold.it/200x200">
-		</a>
 	</div> <!-- col end -->
-	<div class="col-xs-6 col-md-3">
+	<div class="col-md-4">
 		<h2>title</h2>
 		<p>desc</p>
 		<p>0 kr</p>
-		<a href="#" class="thumbnail">
 		<img class='img-responsive' src="http://placehold.it/200x200">
-		</a>
 	</div> <!-- col end -->
 	<?php } ?>
 <?php } ?>

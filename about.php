19b2e64b7164042a48e0f6c65b6cfe0417ff2e8f
malinhanak <malinehanak@gmail.com>
2015-04-13 10:57:54 +0200

    css/about

--- about.php
+++ about.php
@@ -19,7 +19,7 @@ include("inc/header.php"); ?>
 				Säg <strong>Nej</strong> till<p>
 				<span class="krank">Kränkningar</span><p>
 				<span class="disk">Diskriminering</span><p>
-				<span class="mobbning">Mobbning !</span><p>
+				<span class="mobbning">Mobbning</span><p>
 				Vi gör <span class="boardbig">Det!</span>
 				</div>
 			</div>

06ccb8e16bd9c9f983858b67f362411b4d58a7e7
malinhanak <malinehanak@gmail.com>
2015-04-13 09:26:40 +0200

    shirt.php
    Första testet med thumbnails

--- shirts.php
+++ shirts.php
@@ -19,32 +19,58 @@ if($res->num_rows > 0)
 {
 	while($row = $res->fetch_object())
 	{ ?>
-	<div class="col-md-4">
+
+	<!---
+	<div class="col-xs-6 col-md-3">
+		<div class="thumbnail">
 		<h2><?php echo $row->name; ?></h2>
 		<p><?php echo $row->description; ?></p>
 		<p><?php echo $row->price; ?> kr</p>
+	
 		<img class='img-responsive' src="<?php echo $row->img_src; ?>">
-	</div> <!-- col end -->
-	<div class="col-md-4">
-		<h2>title</h2>
-		<p>desc</p>
-		<p>0 kr</p>
-		<img class='img-responsive' src="http://placehold.it/200x200">
-	</div> <!-- col end -->
-	<div class="col-md-4">
-		<h2>title</h2>
-		<p>desc</p>
-		<p>0 kr</p>
-		<img class='img-responsive' src="http://placehold.it/200x200">
-	</div> <!-- col end -->
+		</div>
+	</div> col end // Utkommenterad till databasen är i funktion! -->
 	
 	<?php } ?>
-<?php } ?><div class="col-md-4">
-		<h2>title</h2>
-		<p>desc</p>
-		<p>0 kr</p>
-		<img class='img-responsive' src="http://placehold.it/200x200">
-	</div> <!-- col end -->
+<?php } ?>
+
+		<div class="col-xs-6 col-md-3">
+			<div class="thumbnail">
+			<h2>Title</h2>
+			<p>beskrivning</p>
+			<p>pris (0kr)</p>
+		
+			<img class='img-responsive' src="http://placehold.it/200x200">
+			</div>
+		</div> <!-- col end -->
+		<div class="col-xs-6 col-md-3">
+			<div class="thumbnail">
+			<h2>Title</h2>
+			<p>beskrivning</p>
+			<p>pris (0kr)</p>
+		
+			<img class='img-responsive' src="http://placehold.it/200x200">
+			</div>
+		</div> <!-- col end -->
+		<div class="col-xs-6 col-md-3">
+			<div class="thumbnail">
+			<h2>Title</h2>
+			<p>beskrivning</p>
+			<p>pris (0kr)</p>
+		
+			<img class='img-responsive' src="http://placehold.it/200x200">
+			</div>
+		</div> <!-- col end -->
+		<div class="col-xs-6 col-md-3">
+			<div class="thumbnail">
+			<h2>Title</h2>
+			<p>beskrivning</p>
+			<p>pris (0kr)</p>
+		
+			<img class='img-responsive' src="http://placehold.it/200x200">
+			</div>
+		</div> <!-- col end -->
+
 	</div> <!-- row end -->
 </div> <!-- container end -->
 

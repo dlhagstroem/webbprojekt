0e00e38e6840ffad91608a4d00fa8095ccdb842a
malinhanak <malinehanak@gmail.com>
2015-04-13 10:06:33 +0200

    nav, aboutus, shirts
    nav uppdaterat efter att ändrat shirts.php till paket.php. Lagt in stil
    på aboutus me.

---
+++ about.php
@@ -0,0 +1,22 @@
+<?php 
+$pageTitle ="Om oss";
+include("inc/header.php"); ?>
+
+
+	<div id="mainstyle" class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1>Läsa om oss, våga säga nej till mobbning!</h1>
+			</div>
+		</div> <!-- row end -->
+		<div class="row">
+		<div class="col-md-6">
+			text
+		</div> <!-- col end -->
+		<div class="col-md-6">
+			text
+		</div> <!-- col end -->
+	</div> <!-- row end -->
+</div> <!-- container end -->
+
+<?php include("inc/footer.php"); ?>

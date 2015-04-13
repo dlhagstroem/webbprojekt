ed1552a8485de8bc9bbfee8183185b510244a295
dlhagstroem <dennis.lh@live.se>
2015-04-13 13:37:48 +0200

    swsws

--- paket_details.php
+++ paket_details.php
@@ -16,7 +16,7 @@ if($res->num_rows > 0)
 
 		<?php $row = $res->fetch_object();?>
 		<img class='img-responsive' src="<?php echo $row->img_src; ?>"> <!--större bild?-->
-		<p>Produktnummer:<?php echo {$row->id}?></p><br>
+		<p>Produktnummer: <?php echo {$row->id}?></p><br>
 		<h2>Namn: <?php echo {$row->name}?></h2><br>
 		<h3>Pris: <?php echo {$row->price}?></h3><br>
 		<p>Beskrivning: <?php echo {$row->description}?></p><br>

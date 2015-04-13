c52e35f033c325d5b582758b13b7f47cc74c326c
malinhanak <malinehanak@gmail.com>
2015-04-13 14:44:25 +0200

    paket ahref till details sidan+details fix

--- paket_details.php
+++ paket_details.php
@@ -56,7 +56,19 @@ if($res->num_rows > 0)
 
 		</div> <!-- col end -->
 		<div class="col-md-8">
-		beskrivning
+		beskrivning... Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
+		accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo 
+		inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+		Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
+		sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+		Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, 
+		adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et 
+		dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis 
+		nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex 
+		ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea 
+		voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem 
+		eum fugiat quo voluptas nulla pariatur?
+		<p><a href="#" class="btn btn-primary" role="button">Köp</a></p>
 		</div> <!-- col end -->
 	</div> <!-- row end -->
 </div> <!-- container end -->

cf44cdf0c60003cce1c44fea1e8d5e01a387b762
linusansas <linusansas@gmail.com>
2015-04-16 13:13:08 +0200

    Abouttext och bilder till stödpaket
    Jag la till bilder till stödpaketen i imgmappen och la in texten i
    about.php

--- about.php
+++ about.php
@@ -11,7 +11,25 @@ include("inc/header.php"); ?>
 		</div> <!-- row end -->
 		<div class="row">
 		<div class="col-md-8">
-			text
+			Trots stor uppmärksamhet och hårdare tag kvarstår problemen med mobbning i samhället. Idag blir 60 000 barn och unga utsatta 
+			för mobbning varje dag. I dagsläget finns det också ett flertal stora ickekommersiella organisationer som arbetar mot mobbning. 
+			För att dessa skall kunna drivas behövs det insamlingar från olika håll. Pengarna som samlas in går till att utbilda skolor, 
+			förskolor och idrottsföreningar i hur man arbetar mot mobbning. Målet är att de ska lära sig att själva arbeta aktivt och 
+			långsiktigt mot kränkningar, diskriminering och mobbning. Bris, Rädda barnen, Friends och Noll tolerans är idag ledande 
+			organisation mot mobbning och är våra samarbetspartners.
+			<br>
+			<br>
+			Genom att erbjuda något tillbaka som tack för hjälpen till de som ger en gåva kan vi lättare sprida budskapet och förebygga 
+			mobbning. I dagsläget kan du ge ett bidrag som hjälper olika verksamheter åt rätt riktning när det kommer till att bekämpa 
+			mobbning, men det hade varit lättare att förebygga att unga blir utsatta om vi tillsammans visar att vi är emot kränkningar, 
+			diskriminering och mobbning bland unga. Vår idé är att den som hjälper någon av de insamlande organisationerna med ett bidrag 
+			ska få något tillbaka. Därför erbjuder vi stödpaket som innebär att du ger en gåva till verksamheten och som tack för hjälpen 
+			får du ett gåvobevis och antingen ett halsband, armband eller gosedjur skickat på posten. På så vis kan vi tillsammans visa 
+			att vi inte tolererar att något barn ska behöva utsättas för någon som helst form av mobbning. 
+			<br>
+			<br>
+			Desto fler vi blir desto större motståndskraft!
+
 		</div> <!-- col end -->
 		<div class="col-md-4">
 			<div id="stopbully">

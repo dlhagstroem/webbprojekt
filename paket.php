bdedca585c82ca1d64e763e9b074c9797baeb2f5
malinhanak <malinehanak@gmail.com>
2015-04-17 16:35:37 +0200

    hela paketen klickbara.

--- paket.php
+++ paket.php
@@ -21,15 +21,16 @@ if($res->num_rows > 0)
 	{ ?>
 
 	<div class="col-xs-6 col-md-3">
-		<div class="thumbnail">
-		<a href="paket_details.php?paketId=<?php echo $row->paketId?>" style="text-decoration:None;">
-		<img class='img-responsive' src="<?php echo $row->img_src; ?>"></a>
-			<blockquote style="border:0px;">
-			<p style="min-height:200px;"><?php echo $row->orginfo; ?></p>
-			<p><?php echo $row->included; ?></p>
-			</blockquote>
-		<h4 class="text-center"><?php echo $row->price; ?> kr</h4>
-		</div>
+		<a href="paket_details.php?paketId=<?php echo $row->paketId?>" style="text-decoration:None; color:#333;">
+			<div class="thumbnail">
+			<img class='img-responsive' src="<?php echo $row->img_src; ?>">
+				<blockquote style="border:0px;">
+				<p style="min-height:200px;"><?php echo $row->orginfo; ?></p>
+				<p><?php echo $row->included; ?></p>
+				</blockquote>
+			<h4 class="text-center"><?php echo $row->price; ?> kr</h4>
+			</div>
+		</a>
 	</div>
 	
 	<?php } ?>

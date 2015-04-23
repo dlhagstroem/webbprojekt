f0263607562b1acf848c7de8c7394cdd6bccc02a
dlhagstroem <dennis.lh@live.se>
2015-04-23 10:17:42 +0200

    nu funkar edit

--- edit_paket.php
+++ edit_paket.php
@@ -1,46 +1,44 @@
 <?php
+$pageTitle  = "Ändra produkt";
 include('inc/header.php');
 
-$content = "";
+if(isset($_GET['paketId'])){	
 
-if(isset($_GET['id']))
-{	
-	if(isset($_POST['name']))
-	{
-		$query = <<<END
-		UPDATE products
-		SET name = "{$_POST['name']}", 
-		price = "{$_POST['price']}", 
-		description = "{$_POST['description']}"
-		orginfo = "{$_POST['orginfo']}"
-		included = "{$_POST['included']}"
-		img_src = "{$_POST['img_src']}"
-		WHERE id = "{$_GET['id']}"
-END;
+	if(isset($_POST['name']) && ($_SESSION['admin']) && $_SESSION['admin'] == true){
+		$query = 
+		"INSERT INTO paket(name,price,description,orginfo,included,img_src)
+		VALUES ('" .$_POST['name']. "',
+				'" .$_POST['price']. "',
+				'" .$_POST['description']. "',
+				'" .$_POST['orginfo']. "',
+				'" .$_POST['included']. "',
+				'" .$_POST['img_src']. "')";
+
 		$mysqli->query($query);	
 	}
+}?>
 
-	$query = <<<END
-	SELECT * FROM products
-	WHERE id = "{$_GET['id']}"
-END;
+<?php
+	
+	$query = 
+	"SELECT * FROM paket
+	WHERE paketId = '".$_GET['paketId']."'";
 
 $res = $mysqli->query($query);
-if($res->num_rows > 0)
-{
-	$row = $res->fetch_object();
-$content = <<<END
-	<form method="post" action="edit_product.php?id={$row->id}">
-	<input type="text" name="name" value="{$row->name}">
-	<input type="text" name="price" value="{$row->price}">
-	<textarea name="description">{$row->description}</textarea>
-	<textarea name="orginfo">{$row->orginfo}</textarea>
-	<textarea name="included">{$row->included}</textarea>
-	<input type="text" name="img_src" value="{$row->img_src}">
-	<input type="submit" Value="Spara">
-	</form>
-END;
-}
-}
-echo $content;
+
+	if($res->num_rows > 0)
+	{
+		$row = $res->fetch_object();
 ?>
+
+		<form method="post" action="edit_product.php?id=<?php echo $row->paketId; ?>">
+		<input type="text" name="name" value="<?php echo $row->name; ?>">
+		<input type="text" name="price" value="<?php echo $row->price; ?>">
+		<textarea name="description" value="<?php echo $row->description; ?>"></textarea>
+		<textarea name="orginfo" value="<?php echo $row->orginfo; ?>"></textarea>
+		<textarea name="included" value="<?php echo $row->included; ?>"></textarea>
+		<input type="text" name="img_src" value="<?php echo $row->img_src; ?>">
+		<input type="submit" Value="Spara">
+		</form>
+
+<?php } ?>

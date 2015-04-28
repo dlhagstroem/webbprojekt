1dc6edfff423e1cc872288a84461f198e9ac5224
malinhanak <malinehanak@gmail.com>
2015-04-28 17:25:33 +0200

    edit

--- edit_paket.php
+++ edit_paket.php
@@ -1,3 +1,4 @@
+<?php ini_set("display_errors", 1); error_reporting(E_ALL); ?>
 <?php
 $pageTitle  = "Ändra produkt";
 include('inc/header.php');
@@ -31,14 +32,37 @@ $res = $mysqli->query($query);
 		$row = $res->fetch_object();
 ?>
 
-		<form method="post" action="edit_product.php?id=<?php echo $row->paketId; ?>">
-		<input type="text" name="name" value="<?php echo $row->name; ?>">
-		<input type="text" name="price" value="<?php echo $row->price; ?>">
-		<textarea name="description" value="<?php echo $row->description; ?>"></textarea>
-		<textarea name="orginfo" value="<?php echo $row->orginfo; ?>"></textarea>
-		<textarea name="included" value="<?php echo $row->included; ?>"></textarea>
-		<input type="text" name="img_src" value="<?php echo $row->img_src; ?>">
-		<input type="submit" Value="Spara">
-		</form>
+<div id="mainstyle" class="container" style="width:30%">
+	<div class="row center-block">
+		<div class="col-lg-12 center-block">
+			<h1 class="text-center">Ändra ett paket</h1>
 
-<?php } ?>
+		<form method="post" action="edit_product.php?id=<?php echo $row->paketId; ?>">
+			<div class="form-group">
+			<input type="text" name="name" class="form-control" value="<?php echo $row->name; ?>">
+			</div>
+  			<div class="form-group">
+			<input type="text" name="price" class="form-control" value="<?php echo $row->price; ?>">
+			</div>
+  			<div class="form-group">
+			<textarea name="description" class="form-control" value="<?php echo $row->description; ?>"></textarea>
+			</div>
+			<div class="form-group">
+			<textarea name="orginfo" class="form-control" value="<?php echo $row->orginfo; ?>"></textarea>
+			</div>
+			<div class="form-group">
+			<textarea name="included" class="form-control" value="<?php echo $row->included; ?>"></textarea>
+			</div>
+			<div class="form-group">
+			<input type="text" name="img_src" class="form-control" value="<?php echo $row->img_src; ?>">
+			</div>
+			<button type="submit" class="btn btn-default center-block">Spara</button>
+			</form>
+
+<?php } ?>	
+
+		</div>
+	</div> <!-- row end -->
+</div> <!-- container end -->
+
+<?php include("inc/footer.php"); ?>
